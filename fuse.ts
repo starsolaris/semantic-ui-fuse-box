a11671de6bd53fb0acdb3b7c2275c05f1c282ae6
starsolaris <itmyaddress@gmail.com>
2020-03-29 20:16:57 +0300

    semantic-ui-less with fuse-box example

---
+++ fuse.ts
@@ -0,0 +1,44 @@
+import {fusebox, pluginLess} from "fuse-box";
+import {pluginTypeChecker} from "fuse-box-typechecker";
+import * as path from "path";
+
+const fuse = fusebox({
+  target: "browser",
+  entry: 'src/index.tsx',
+  webIndex: {
+    publicPath: "./",
+    template: 'src/index.html'
+  },
+  devServer: true,
+  sourceMap: true,
+  cache: {
+    root: "build/.cache"
+  },
+  plugins: [
+    pluginLess("*.less", {
+      stylesheet: {
+        macros: {
+          '../../theme.config': path.resolve(__dirname, "src/semantic-ui/theme.config"),
+          'themes/grey': path.resolve(__dirname, "src/semantic-ui/themes/grey"),
+          '~semantic-ui-less': path.resolve(__dirname, 'node_modules/semantic-ui-less'),
+          '@spritePath': path.resolve(__dirname, 'node_modules/semantic-ui-less/themes/default/assets/images/flags.png'),
+          '@{fontPath}/@{fontName}': path.resolve(__dirname, 'node_modules/semantic-ui-less/themes/default/assets/fonts/icons'),
+          '@{fontPath}/@{outlineFontName}': path.resolve(__dirname, 'node_modules/semantic-ui-less/themes/default/assets/fonts/outline-icons'),
+          '@{fontPath}/@{brandFontName}': path.resolve(__dirname, 'node_modules/semantic-ui-less/themes/default/assets/fonts/brand-icons')
+        }
+      }
+    }),
+    pluginTypeChecker({
+      tsConfig: './tsconfig.json',
+      name: 'TypeChecker App'
+    })
+  ],
+});
+
+fuse.runDev({
+  bundles: {
+    distRoot: "build/browser",
+    app: "app.js",
+    vendor: "vendor.js"
+  }
+});
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Button} from "semantic-ui-react";
+
+import 'semantic-ui-less/semantic.less'
+
+ReactDOM.render(
+  <div>
+    <Button primary>Primary</Button>
+  </div>,
+  document.getElementById("root")
+);
