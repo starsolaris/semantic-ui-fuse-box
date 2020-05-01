3a4184d5cc6cb3fe108995d702d6433357281358
starsolaris <itmyaddress@gmail.com>
2020-05-01 22:32:53 +0300

    add Some component

---
+++ src/Some/Some.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./some.css";
+
+const Some = () => {
+  return (<div className="some">Some text</div>);
+};
+
+export default Some;
--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Button} from "semantic-ui-react";
+import Some from "./Some";
 
 import 'semantic-ui-less/semantic.less'
 
 ReactDOM.render(
   <div>
     <Button primary>Primary</Button>
+    <Some/>
   </div>,
   document.getElementById("root")
 );
